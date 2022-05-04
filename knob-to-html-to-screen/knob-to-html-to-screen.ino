35067b9600db9d78b9355cacea642878499cf26a
louisafriederike <70212792+louisafriederike@users.noreply.github.com>
2022-05-04 16:54:55 +0200

    icons

--- knob-to-html-to-screen/knob-to-html-to-screen.ino
+++ knob-to-html-to-screen/knob-to-html-to-screen.ino
@@ -2,6 +2,15 @@
 #include <U8g2lib.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <ESP8266WiFi.h>
+#ifndef STASSID
+#define STASSID "Galaxy A51564F"
+#define STAPSK  "kalq8869"
+#endif
+
+const char* ssid     = STASSID;
+const char* password = STAPSK;
+
 
 int percent = 0;
 int prevPercent = 0;
@@ -10,6 +19,20 @@ U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 void setup(void) {
   Serial.begin( 9600 );
+  Serial.println("helooooastaaaarttt");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
   u8g2.begin();
 }
 
