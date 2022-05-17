12dbdf9c5f0c5ae644391bfad89be0adfbd5a0b8
louisafriederike <70212792+louisafriederike@users.noreply.github.com>
2022-05-18 00:44:05 +0200

    full update gamelay

---
+++ public/audio.js
@@ -0,0 +1,77 @@
+
+function playTrack1(){
+    var Track1 = new Audio('audio/unlocked.mp3');
+    Track1.play();
+}
+
+function playTrack2(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("From here I can see a gate, i follow it.");
+    music2.play();
+}
+
+function breadCrumb1(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("I am standing outside the gallery space, hearing an unusual sound.");
+    music2.play();
+}
+
+function breadCrumb2(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("I follow it down the sidestreet that leads me past a parking garage covered in ivy.");
+    music2.play();
+}
+
+function breadcrumb3(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("From here I can see an open gate, but I do not enter. Just to the right of it, there is a path leading me to a high voltage metal door.");
+    music2.play();
+}
+
+function breadCrumb4(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("I turn around, back to the gate. I still don't enter, but walk towards strings of lights and noise.");
+    music2.play();
+}
+
+function breadCrumb5(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("I keep on walking without making a turn, until I reach a massive pile of sand.");
+    music2.play();
+}
+
+function breadcrumb6(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("I move swiftly along the construction fence, past the entrance of a church. I can see you now. I cross the street, walking towards benches covered in overgrown bushes. This is where we meet.");
+    music2.play();
+}
+
+function breadCrumb7(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("I head towards the big water, but just before I reach the busy road, I take a turn left into a narrow side street.");
+    music2.play();
+}
+
+function breadCrumb8(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("From here I can see a big white building, with many round windows and lights.");
+    music2.play();
+}
+
+function breadcrumb9(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("I walk past it and head left, past you until I almost cannot walk any further.");
+    music2.play();
+}
+
+function breadcrumb10(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("I run up the stairs, up and down, trying to shake you off.");
+    music2.play();
+}
+
+function breadcrumb11(){
+    var music2 = new Audio('audio/breadcrumb.mp3');
+    $("#objective").text("I can see large metal structures by the water.");
+    music2.play();
+}
--- public/fence.js
+++ public/fence.js
@@ -1,32 +1,97 @@
 window.onload = function() {
     var startPos;
-    var startPosLat;
-    var startPosLong;
+    var node1Lat;
+    var node1Long;
+    var node2Lat;
+    var node2Long;
+    var node3Lat;
+    var node3Long;
+    var node4Lat;
+    var node4Long;
+    var node5Lat;
+    var node5Long;
+    var node6Lat;
+    var node6Long;
     var distance;
+    var distance2;
   
     if (navigator.geolocation) {
 
-      startPosLat = 51.91575615609297;
-      startPosLong = 4.476337286922251;
+      node1Lat = 51.916080703822985;
+      node1Long = 4.476145158543535;
 
-      $("#startLat").text(startPosLat);
-      $("#startLon").text(startPosLong);
+      node2Lat = 51.91620529794022;
+      node2Long = 4.47790731771267;
+
+      node3Lat = 51.91435892487735;
+      node3Long = 4.480129527881552;
+
+      node4Lat = 51.91571040166534;
+      node4Long = 4.480724812513352;
+
+      node5Lat = 51.91697272192186;
+      node5Long = 4.4838308105517815;
+
+      node6Lat = 51.916080703822985;
+      node6Long = 4.476145158543535;
+
+      message = document.querySelector('#on-location-track');
+      node01active = document.querySelector('#objective');
+      nav = document.querySelector('.nav-track');
+
+      $("#startLat").text(node1Lat);
+      $("#startLon").text(node1Long);
+      $("#startLat2").text(node2Lat);
+      $("#startLon2").text(node2Long);
   
       navigator.geolocation.watchPosition(function(position) {
         $("#currentLat").text(position.coords.latitude);
         $("#currentLon").text(position.coords.longitude);
-
-        distance = calculateDistance(startPosLat, startPosLong,position.coords.latitude, position.coords.longitude)
+      
+      
+        if(localStorage.getItem('gateway')== '1'){
+          distance2 = calculateDistance(node2Lat, node2Long,position.coords.latitude, position.coords.longitude)
+          $("#distance").text(distance2);
+      }
+        distance = calculateDistance(node1Lat, node1Long,position.coords.latitude, position.coords.longitude)
         $("#distance").text(distance);
-
+        distance2 = calculateDistance(node2Lat, node2Long,position.coords.latitude, position.coords.longitude)
+        $("#distance2").text(distance2);
+        distance3 = calculateDistance(node3Lat, node3Long,position.coords.latitude, position.coords.longitude)
+        $("#distance3").text(distance);
+        distance4 = calculateDistance(node4Lat, node4Long,position.coords.latitude, position.coords.longitude)
+        $("#distance4").text(distance2);
+        distance5 = calculateDistance(node5Lat, node5Long,position.coords.latitude, position.coords.longitude)
+        $("#distance5").text(distance);
+        distance6 = calculateDistance(node6Lat, node6Long,position.coords.latitude, position.coords.longitude)
+        $("#distance6").text(distance2);
+        
         if(distance < .01){
-          $("#message").text("You are within reach of Gateway 1.")
+          $("#message").text(" >>> you have entered gateway 1");
+          $("#objective").text("turn the knob to 55 and wait for a moment. the creature will reveal a 5 digit code. enter it in the port above to unlcok the first dream log.");
+          playTrack1();
+         
+          message.style.visibility = 'visible';
+          nav.style.visibility = 'hidden';
         }else if(distance > .01){
-          $("#message").text("You are outside reach of Gateway 1.")
+          $("#message").text("outside reach of gateway 1");
+          $("#objective").text("Enter S/ash Gallery to step into the game.");
+        }
+
+        if(distance2 < .01){
+          playTrack1();
+          $("#message2").text("You have entered Gateway 2.");
+          $("#objective").text("You were everywhere and in everything, and every time, I instinctively new where to find you here. Nested beneath the asphalt, you felt the weight of my every step as I felt the weight of your gaze on my back. Everywhere. I didn't not yet realise the meaning and weight. Space and time work differently in this world of yours. I have waited lifetimes for you, sitting right here on this bench. Lifetimes, turning into fractures of moments as I blink my eyes open. But I started to remember more and more. Right here is the home you built for us. I recognize the shapes of the walls and the sounds of the water. The canary yellow. It was here. It was right here. ");
+          message.style.visibility = 'visible';
+          nav.style.visibility = 'hidden';
+        }else if(distance2 > .01){
+          $("#message2").text("You are outside reach of Gateway 2.");
         }
       });
     }
   };
+
+  
   
   // Reused code - copyright Moveable Type Scripts - retrieved May 4, 2010.
   // http://www.movable-type.co.uk/scripts/latlong.html
